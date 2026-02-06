fb0ac7ee3c7750d1aab359d7df4a9e06fecfd92f
SecureAuditX <abdulkarimumar86@gmail.com>
2026-02-06 17:58:53 +0800

    styling the type and weakness of the pokemon card character

--- components/PokemonCard.js
+++ components/PokemonCard.js
@@ -1,5 +1,21 @@
 import {View, Text, StyleSheet, Platform, Image} from 'react-native'
 
+// Styles types base on the pokemon
+const getTypeDetailts = (type) => {
+    switch (type.toLowerCase()) {
+        case "electric":
+            return {borderColor: "#FFD700", emoji: "⚡"};
+        case "water":
+            return {borderColor: "#6493EA", emoji: "💧"};
+        case "fire":
+            return {borderColor: "#FF5733", emoji: "🔥"};
+        case "grass":
+            return {borderColor: "#66CC66", emoji: "🌿"};
+        default:
+            return {borderColor: "#A0A0A0", emoji: "❓"};
+    }
+}
+
 export default function PokemonCard({
     name,
     image,
@@ -9,6 +25,8 @@ export default function PokemonCard({
     weaknesses
 }) {
     
+    // Invoke the style type function
+    const {borderColor, emoji} = getTypeDetailts(type)
     return (
         <View style={styles.card}>
             <View style={styles.nameContainer}>
@@ -21,21 +39,25 @@ export default function PokemonCard({
             accessibilityLabel={`${name} pokemon`}
             />
 
-            <View>
-                <Text>{type}</Text>
+            <View style={styles.typeContainer}>
+                <View style={[styles.badge, {borderColor}]}>
+                    <Text style={styles.typeEmoji}>{emoji}</Text>
+                    <Text style={styles.typeText}>{type}</Text>
+                </View>
             </View>
 
-            <View>
-                <Text>Moves: {moves.join(", ")}</Text>
+            <View style={styles.movesContainer}>
+                <Text style={styles.movesText}>Moves: {moves.join(", ")}</Text>
             </View>
 
-            <View>
-                <Text>Weakness: {weaknesses.join(", ")}</Text>
+            <View style={styles.weaknessContainer}>
+                <Text style={styles.weaknessText}>Weakness: {weaknesses.join(", ")}</Text>
             </View>
         </View>
     )
 }
 
+// Styling
 const styles = StyleSheet.create({
     card:{
         backgroundColor: "white",
@@ -72,5 +94,39 @@ const styles = StyleSheet.create({
         height: 200,
         marginBottom: 16,
         resizeMode: "contain"
+    },
+    typeContainer: {
+        alignItems: "center",
+        marginBottom: 40
+    },
+    badge: {
+        flexDirection: "row",
+        alignItems: "center",
+        paddingVertical: 6,
+        paddingHorizontal: 12,
+        borderRadius: 20,
+        borderWidth: 4
+    },
+    typeEmoji: {
+        fontSize: 30,
+        marginRight: 12
+    },
+    typeText: {
+        fontSize: 22,
+        fontWeight: "bold"
+    },
+    movesContainer: {
+        marginBottom: 16,
+    },
+    movesText: {
+        fontSize: 22,
+        fontWeight: "bold"
+    },
+    weaknessContainer: {
+        marginBottom: 8
+    },
+    weaknessText: {
+        fontSize: 22,
+        fontWeight: "bold"
     }
 })
